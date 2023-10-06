56785f33a463d99e15e03cbcf86383873720f592
icodezjb <icodezjb@users.noreply.github.com>
2023-10-07 00:40:58 +0800

    Replace HelloToken -> SoSwapMessage

--- programs/hello-token/src/cross.rs
+++ programs/hello-token/src/cross.rs
@@ -36,6 +36,17 @@ pub struct NormalizedSwapData {
     pub call_data: Vec<u8>
 }
 
+#[derive(PartialEq, Debug, Clone)]
+pub struct NormalizedWormholeData {
+    // destination wormhole chain id
+    pub dst_wormhole_chain_id: u16,
+    // gas price of the target chain
+    pub dst_max_gas_price_in_wei_for_relayer: U256,
+    // payment required for aptos coin
+    pub wormhole_fee: U256,
+    // destination chain sodiamond address
+    pub dst_so_diamond: Vec<u8>,
+}
 
 impl NormalizedSoData {
     pub fn padding_so_data(
@@ -132,16 +143,15 @@ impl NormalizedSwapData {
         }
     }
 
-    pub fn encode_normalized_swap_data(swap_data: &mut Vec<NormalizedSwapData>) -> Vec<u8> {
+    pub fn encode_normalized_swap_data(swap_data: &Vec<NormalizedSwapData>) -> Vec<u8> {
         let mut data = Vec::<u8>::new();
-        swap_data.reverse();
 
         let swap_len = swap_data.len();
         if swap_len > 0 {
             serde::serialize_u64(&mut data, swap_len as u64)
         };
 
-        while let Some(d) = swap_data.pop() {
+        for d in swap_data {
             serde::serialize_vector_with_length(&mut data, &d.call_to);
             serde::serialize_vector_with_length(&mut data, &d.approve_to);
             serde::serialize_vector_with_length(&mut data, &d.sending_asset_id);
@@ -206,10 +216,70 @@ impl NormalizedSwapData {
 }
 
 
+impl NormalizedWormholeData {
+    pub fn encode_normalized_wormhole_data(wormhole_data: &NormalizedWormholeData) -> Vec<u8> {
+        let mut data = Vec::<u8>::new();
+
+        serde::serialize_u16(&mut data, wormhole_data.dst_wormhole_chain_id);
+        serde::serialize_u256(&mut data, wormhole_data.dst_max_gas_price_in_wei_for_relayer);
+        serde::serialize_u256(&mut data, wormhole_data.wormhole_fee);
+        serde::serialize_vector_with_length(&mut data, &wormhole_data.dst_so_diamond);
+        data
+    }
+
+    pub fn decode_normalized_wormhole_data(data: &[u8]) -> Self {
+        let data_len = data.len();
+        assert!(data_len > 0, "EINVALID_LENGTH");
+        let mut index = 0;
+        let mut next_len;
+
+        next_len = 2;
+        let dst_wormhole_chain_id = serde::deserialize_u16(&data[index..index + next_len]);
+        index = index + next_len;
+
+        next_len = 32;
+        let dst_max_gas_price_in_wei_for_relayer = serde::deserialize_u256(&data[index..index + next_len]);
+        index = index + next_len;
+
+        next_len = 32;
+        let wormhole_fee = serde::deserialize_u256(&data[index..index + next_len]);
+        index = index + next_len;
+
+        next_len = (8 + serde::get_vector_length(&data[index..index + 8])) as usize;
+        let dst_so_diamond = serde::deserialize_vector_with_length(&data[index..index + next_len]);
+        index = index + next_len;
+
+        assert!(index == data_len, "EINVALID_LENGTH");
+
+        NormalizedWormholeData {
+            dst_wormhole_chain_id,
+            dst_max_gas_price_in_wei_for_relayer,
+            wormhole_fee,
+            dst_so_diamond
+        }
+    }
+}
+
 #[cfg(test)]
 pub mod test {
     use hex_literal::hex;
     use super::*;
+
+    #[test]
+    fn test_wormhole_data() {
+        let wormhole_data = NormalizedWormholeData {
+            dst_wormhole_chain_id: 1,
+            dst_max_gas_price_in_wei_for_relayer: U256::from(10000u32),
+            wormhole_fee: U256::from(2389u32),
+            dst_so_diamond: hex!("2dA7e3a7F21cCE79efeb66f3b082196EA0A8B9af").to_vec()
+        };
+        let encode_data = NormalizedWormholeData::encode_normalized_wormhole_data(&wormhole_data);
+
+        let data = hex!("00010000000000000000000000000000000000000000000000000000000000002710000000000000000000000000000000000000000000000000000000000000095500000000000000142da7e3a7f21cce79efeb66f3b082196ea0a8b9af").to_vec();
+
+        assert_eq!(data, encode_data);
+        assert_eq!(NormalizedWormholeData::decode_normalized_wormhole_data(&data), wormhole_data);
+    }
 
     #[test]
     fn test_serde_so_data() {
@@ -252,7 +322,7 @@ pub mod test {
             }
         ];
 
-        let encode_data = NormalizedSwapData::encode_normalized_swap_data(&mut swap_data.clone());
+        let encode_data = NormalizedSwapData::encode_normalized_swap_data(&swap_data);
         let data = hex!("000000000000000200000000000000204e9fce03284c0ce0b86c88dd5a46f050cad2f4f33c4cdd29d98f501868558c8100000000000000204e9fce03284c0ce0b86c88dd5a46f050cad2f4f33c4cdd29d98f501868558c81000000000000001a3078313a3a6170746f735f636f696e3a3a4170746f73436f696e00000000000000163078313a3a6f6d6e695f6272696467653a3a5842544300000000000000000000000000000000000000000000000000000002127b390000000000000000583078346539666365303332383463306365306238366338386464356134366630353063616432663466333363346364643239643938663530313836383535386338313a3a6375727665733a3a556e636f7272656c617465640000000000000014957eb0316f02ba4a9de3d308742eefd44a3c17190000000000000014957eb0316f02ba4a9de3d308742eefd44a3c171900000000000000142514895c72f50d8bd4b4f9b1110f0d6bd2c975260000000000000014143db3ceefbdfe5631add3e50f7614b6ba708ba700000000000000000000000000000000000000000000000000000001caf4ad0000000000000000146ce9e2c8b59bbcf65da375d3d8ab503c8524caf7").to_vec();
 
         assert_eq!(data, encode_data);
--- programs/hello-token/src/state/mod.rs
+++ programs/hello-token/src/state/mod.rs
@@ -1,6 +1,6 @@
-pub use foreign_contract::*;
+pub use foreign_contract::ForeignContract;
 pub use redeemer_config::*;
-pub use sender_config::*;
+pub use sender_config::{OutboundTokenBridgeAddresses, SenderConfig};
 
 pub mod foreign_contract;
 pub mod redeemer_config;
